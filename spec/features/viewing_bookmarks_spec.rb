af33d9ef2b3a59e22dbed3d7aab0a34b97eecc05
Daniel Watts <d.watts15@outlook.com>
2022-02-08 17:34:43 +0000

    Finished challenge 9. setup_test_database is running correctly and passing tests.

--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -1,7 +1,24 @@
 require 'pg'
 
+# feature 'Viewing bookmarks' do
+#   scenario 'A user can see bookmarks' do
+#     visit('/bookmarks')
+
+#     expect(page).to have_content "http://www.makersacademy.com"
+#     expect(page).to have_content "http://www.destroyallsoftware.com"
+#     expect(page).to have_content "http://www.google.com"
+#   end
+# end
+
 feature 'Viewing bookmarks' do
   scenario 'A user can see bookmarks' do
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+
+    # Add the test data
+    connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com');")
+    connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.destroyallsoftware.com');")
+    connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.google.com');")
+
     visit('/bookmarks')
 
     expect(page).to have_content "http://www.makersacademy.com"
--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -2,7 +2,9 @@ require 'pg'
 
 p "Setting up test database..."
 
-connection = PG.connect(dbname: 'bookmark_manager_test')
+def setup_test_database
+  connection = PG.connect(dbname: 'bookmark_manager_test')
 
-# Clear the bookmarks table
-# connection.exec("TRUNCATE bookmarks;")
+  # Clear the bookmarks table
+  connection.exec("TRUNCATE bookmarks;")
+end
