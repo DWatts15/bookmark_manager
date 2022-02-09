fe447a21683a98066c4b661bc40fb92a0c0aae1f
Daniel Watts <d.watts15@outlook.com>
2022-02-09 15:55:35 +0000

    Completed challenge 14, before refactor.

--- app.rb
+++ app.rb
@@ -34,5 +34,20 @@ class BookmarkManager < Sinatra::Base
     redirect '/bookmarks'
   end
 
+  get '/bookmarks/:id/edit' do
+    @bookmark_id = params[:id]
+    erb :'bookmarks/edit'
+  end
+
+  patch '/bookmarks/:id' do
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+    connection.exec_params(
+      "UPDATE bookmarks SET url = $1, title = $2 WHERE id = $3",
+      [ params[:url], params[:title], params[:id] ]
+    )
+  
+    redirect('/bookmarks')
+  end
+
   run! if app_file == $0
 end
