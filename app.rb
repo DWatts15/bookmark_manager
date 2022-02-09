406cb09b7db6198b0d72098b6d38ecd252893c0e
Daniel Watts <d.watts15@outlook.com>
2022-02-09 16:41:32 +0000

    Challenge 14 now refactoring.

--- app.rb
+++ app.rb
@@ -35,18 +35,18 @@ class BookmarkManager < Sinatra::Base
   end
 
   get '/bookmarks/:id/edit' do
-    @bookmark_id = params[:id]
+    @bookmark = Bookmark.find(id: params[:id])
     erb :'bookmarks/edit'
   end
 
   patch '/bookmarks/:id' do
-    connection = PG.connect(dbname: 'bookmark_manager_test')
-    connection.exec_params(
-      "UPDATE bookmarks SET url = $1, title = $2 WHERE id = $3",
-      [ params[:url], params[:title], params[:id] ]
-    )
-  
-    redirect('/bookmarks')
+    Bookmark.update(id: params[:id], title: params[:title], url: params[:url])
+    redirect '/bookmarks'
+  end
+
+  get '/bookmarks/:id/edit' do
+    @bookmark = Bookmark.find(id: params[:id])
+    erb :"bookmarks/edit"
   end
 
   run! if app_file == $0
--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -48,4 +48,27 @@ class Bookmark
     end
     connection.exec_params("DELETE FROM bookmarks WHERE id = $1", [id])
   end
+
+  def self.update(id:, url:, title:)
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+    else
+      connection = PG.connect(dbname: 'bookmark_manager')
+    end
+    result = connection.exec_params(
+      "UPDATE bookmarks SET url = $1, title = $2 WHERE id = $3 RETURNING id, url, title;",
+      [url, title, id]
+    )
+    Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
+  end
+
+  def self.find(id:)
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+    else
+      connection = PG.connect(dbname: 'bookmark_manager')
+    end
+    result = connection.exec_params("SELECT * FROM bookmarks WHERE id = $1;", [id])
+    Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
+  end
 end
